c7875babae88a9f037a2219ce3a881c13c0ecf7c
bei931212 <837765980@qq.com>
2017-04-26 11:07:10 +0800

    订单状态
    订单状态

---
+++ js/js.js
@@ -0,0 +1,191 @@
+function fMan(status)
+        {
+            var sStatus="";           
+            switch (status)
+            {
+                case "1":
+                {
+                    sStatus="<i class=grey>白班</i>";
+                    break;
+                }
+                case "2":
+                {
+                    sStatus="<i class=deny>晚班</i>";
+                    break;
+                }
+                default :
+                {
+                    break;
+                }
+            }
+            document.write(sStatus);
+        }
+		
+		 function fSort(status)
+        {
+            var sStatus="";           
+            switch (status)
+            {
+                case "1":
+                {
+                    sStatus="客户";
+                    break;
+                }
+                case "2":
+                {
+                    sStatus="平台";
+                    break;
+                }
+				 case "3":
+                {
+                    sStatus="商家";
+                    break;
+                }
+				 case "4":
+                {
+                    sStatus="物流";
+                    break;
+                }
+                default :
+                {
+                    break;
+                }
+            }
+            document.write(sStatus);
+        }
+		
+		function fs(status)
+        {
+            var sStatus="";           
+            switch (status)
+            {
+                case "1":
+                {
+                    sStatus="跟进中";
+                    break;
+                }
+                case "2":
+                {
+                    sStatus="待回访";
+                    break;
+                }
+				 case "3":
+                {
+                    sStatus="处理中";
+                    break;
+                }
+				 case "4":
+                {
+                    sStatus="已处理";
+                    break;
+                }
+                default :
+                {
+                    break;
+                }
+            }
+            document.write(sStatus);
+        }
+		
+		function qs(status)
+        {
+            var sStatus="";           
+            switch (status)
+            {
+                case "1":
+                {
+                    sStatus="无理由退换货";
+                    break;
+                }
+                case "2":
+                {
+                    sStatus="撤销申请";
+                    break;
+                }
+				case "3":
+                {
+                    sStatus="查物流";
+                    break;
+                }
+				 case "4":
+                {
+                    sStatus="信息错误";
+                    break;
+                }
+				 case "5":
+                {
+                    sStatus="虚假发货";
+                    break;
+                }				 
+                default :
+                {
+                    break;
+                }
+            }
+            document.write(sStatus);
+        }
+		
+		function fList(status)
+        {
+            var sStatus="";           
+            switch (status)
+            {
+               
+			   
+					      case "47":
+                {
+                     sStatus="叶蕾";
+                    break;
+                }
+                        
+					      case "46":
+                {
+                     sStatus="周玲铭";
+                    break;
+                }
+                        
+					      case "45":
+                {
+                     sStatus="麻冬萍";
+                    break;
+                }
+                default :
+                {
+                    break;
+                }
+            }
+            document.write(sStatus);
+        }
+		
+		function fmm(status)
+        {
+            var sStatus="";           
+            switch (status)
+            {
+               
+			   
+					      case "21":
+                {
+                     sStatus="楼丁畅";
+                    break;
+                }
+                        
+					      case "20":
+                {
+                     sStatus="王威钧";
+                    break;
+                }
+                        
+					      case "19":
+                {
+                     sStatus="郑纯正";
+                    break;
+                }                      
+					     
+                default :
+                {
+                    break;
+                }
+            }
+            document.write(sStatus);
+        }
